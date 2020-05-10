34f4b4138f4567f896cd2095a77019d95c5a833f
Senthil Palanisamy <senthillihtnes1994@gmail.com>
2020-05-10 11:48:33 -0500

    Implemented a more intelligent strategy for map matching

--- assignment_2/source/database_upload.py
+++ assignment_2/source/database_upload.py
@@ -1,6 +1,8 @@
 import csv, sqlite3
 import pandas as pd
 import utm
+import math
+import numpy as np
 
 class databaseReader:
   PVID_INDEX = 0
@@ -78,6 +80,67 @@ def map_match(links, probe):
 
     return closest_link
 
+def return_gaussian_probability(error, std):
+    probability = 1/ (np.sqrt(2 * np.pi) * std) * np.exp(-(error)**2/(2 * std**2))
+    return probability
+
+
+def map_match_line_interpolated(links, probe):
+    # Design map matching algorithm and return the mathched link
+
+    probe_lat = probe[2]
+    probe_lon = probe[3]
+    px, py, probe_zone_num, probe_zone_let = utm.from_latlon(float(probe_lat), float(probe_lon))
+    closest_link = []
+    probe_heading = probe[-1]
+
+    d_std = 5.0
+    h_std = 1.047 # 60 degrees
+
+    sub_link_indices = []
+    all_link_probs = []
+
+    for link in links:
+        shape_points_list = link[14].split('|')
+        direction = link[5]
+        lines = []
+        first_point = []
+        cartesian_shape_points = []
+        for shape_pt in shape_points_list:
+            shape_latitude, shape_longitude = shape_pt.split('/')[:2]
+            utm_e, utm_n, _, _= utm.from_latlon(float(shape_latitude), float(shape_longitude))
+            cartesian_shape_points.append([utm_e, utm_n])
+
+        previous_pt = cartesian_shape_points[0]
+        all_sublink_probs = []
+        for current_pt in cartesian_shape_points[1:]:
+            vx1, vy1 = previous_pt
+            vx2, vy2 = current_pt
+            t = ((vx1 - px) * (vx2 - vx1) + (vy1 - py) * (vy2 - vy1)) / ((vx2-vx1)**2 + (vy2-vy1)**2)
+            if (0 <= t <= 1):
+                dst = abs((vx2-vx1)*(vy1-py) - (vy2-vy1) * (vx1-px)) / (math.sqrt(vx2-vx1) + (vy2-vy1)**2)
+            else:
+                d1 = math.sqrt((vx1-px)**2 + (vy1-py)**2)
+                d2 = math.sqrt((vx2-px)**2 + (vy2-py)**2)
+                dst = min(d1, d2)
+            if direction == 'F':
+                heading = math.atan2(vy2-vy1, vx2-vx1)
+            else:
+                heading = math.atan2(vy1-vy2, vx1-vx2)
+            h_error  = heading - math.radians(probe_heading)
+            heading_difference = math.atan2(np.sin(h_error), np.cos(h_error))
+            p_link_probability = return_gaussian_probability(dst, d_std) *\
+                                 return_gaussian_probability(heading_difference, h_std)
+            all_sublink_probs.append(p_link_probability)
+        max_index = all_sublink_probs.index(max(all_sublink_probs))
+        sub_link_indices.append(max_index)
+        all_link_probs.append(all_sublink_probs[max_index])
+    matched_link_index = all_link_probs.index(max(all_link_probs))
+    sublink_index = sub_link_indices[matched_link_index]
+    closest_link = links[matched_link_index]
+ 
+    return closest_link, sublink_index
+
 def calculate_slope(probe_data, previous_probe_data):
     # calculate the slope of the point with probe data
 
@@ -108,7 +171,8 @@ def analyse_probedata(probe_csv_path, op_csv_path):
         relevant_links = linkdatabase.filter_matching_links(latitude, longitude)
 
         # core function
-        matched_link = map_match(relevant_links, probe_data_formatted)
+        # matched_link = map_match(relevant_links, probe_data_formatted)
+        matched_link, sublink_index = map_match_line_interpolated(relevant_links, probe_data_formatted)
         if previous_probe_point[0] == probe_data_formatted[0] and\
            previous_link_match == matched_link:
           slope_value = calculate_slope(probe_data_formatted, previous_probe_point)
